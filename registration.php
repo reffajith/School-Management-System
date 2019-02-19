d3c4df615dc27a205fe91239479fb1bdb5566a57
Ajith Sebastian <reffajith@gmail.com>
2019-02-19 12:50:55 +0100

    Edited Registration Page

--- registration.php
+++ registration.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,7 +8,6 @@
   <meta name="author" content="">
 
   <title>Gambit User - Registration</title>
-
   <!-- Custom fonts for this template-->
   <link href="../resources/fontawesome/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -32,7 +29,7 @@
           <div class="col-lg-7">
             <div class="p-5">
               <div class="text-center">
-                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                <h1 class="h4 text-gray-900 mb-4">Registration!</h1>
               </div>
               <form class="user">
                 <div class="form-group row">
